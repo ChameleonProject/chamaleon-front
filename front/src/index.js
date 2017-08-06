a4fed810576e35df3190f262a782842d8224c02b
Pedro H. Santiago <pedroh_s_@hotmail.com>
2017-08-05 23:19:47 -0300

    renamed root

---
+++ front/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+import Home from './components/Home'
+import Groups from './components/Groups'
+
+import reducers from './reducers'
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+      <Router history={history}>
+        <Route path="/" component={Home}>
+        <Route path="/groups" component={Groups}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
